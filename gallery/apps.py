f38dbb5ddf1ac6874ae9c1a1be6ef4c0d6d62a06
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-27 10:16:30 -0400

    templates displaying successfully

--- gallery/apps.py
+++ gallery/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class GalleryConfig(AppConfig):
-    name = 'Gallery'
+    name = 'gallery'
--- pics/urls.py
+++ pics/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^Gallery/',include('Gallery.urls'))
+    url(r'^gallery/',include('Gallery.urls'))
 ]
