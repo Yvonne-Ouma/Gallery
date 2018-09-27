0c4a00487e23452afc1f191baee2f5f64cf475d2
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-27 15:29:46 -0400

    create a class for images

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class Photoeditor(models.Model):
+    first_name = models.CharField( max_length=50)
+    last_name = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.first_name
+
+    def save_Photoeditor():
+        self.save() 
+
+           
+
+    class Meta:
+        ordering = ['first_name']
+
+class Category(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+    
+
+class Image(models.Model):
+    title = models.CharField(max_length = 60)
+    name = models.CharField(max_length = 60)
+    description = models.CharField(max_length = 90)
+    photoeditor = models.ForeignKey(Photoeditor)
+    category = models.ManyToManyField(Category)
+
+
+    def __str__(self):
+        return self.title
+
+    def save_Image():
+        self.save()
+
+    def delete_Image():
+        self.delete()    
+
+
+
+
--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,54 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
     return render(request, 'gallery.html')
+
+def posts_of_day(request):
+    date = dt.date.today()
+
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+def convert_dates(dates):
+    
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day 
+
+# view function for present posts from past days
+def past_days_posts(request,past_date):
+    try:
+    
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+      
+    except ValueError:
+        # Raise 404 error when ValueError is thrown    
+        raise Http404()
+        assert False
+
+
+        day = convert_dates(date)
+        html = f'''
+        <html>
+            <body>
+                <h1>Posts for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+        return HttpResponse(html)
