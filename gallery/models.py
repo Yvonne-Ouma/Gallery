0e7c22424aaa7100dbbe13e89514316385748e45
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-27 16:22:53 -0400

    admin is working

--- gallery/models.py
+++ gallery/models.py
@@ -4,14 +4,16 @@ from django.db import models
 class Photoeditor(models.Model):
     first_name = models.CharField( max_length=50)
     last_name = models.CharField(max_length = 50)
+    phone_number = models.CharField(max_length = 10,blank =True)
 
     def __str__(self):
         return self.first_name
 
-    def save_Photoeditor():
+    def save_photoeditor():
         self.save() 
 
-           
+    def delete_Photoeditor():
+        self.delete()       
 
     class Meta:
         ordering = ['first_name']
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from .models import Photoeditor,Category,Image
+
+class PhotoeditorTestClass(TestCase):
+
+    #Set up method
+    def setUp(self):
+        self.yvon = Photoeditor(first_name = 'Yvonne', last_name = 'Ouma')
+
+    # Test instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.yvon,Photoeditor)) 
+
+    def test_save_method(self):
+        self.yvon.save_photoeditor()
+        yvon = Photoeditor.objects.all()
+        self.assertTrue(len(yvon) > 0)     
