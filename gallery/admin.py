df52ae99a0876045e17a9423a81504d6a0981350
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-27 16:40:28 -0400

    modified my admin.py

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Photoeditor,Category,Image
 
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('Category',)
+
 # Register your models here.
 admin.site.register(Photoeditor)
 admin.site.register(Category)
-admin.site.register(Image)
+admin.site.register(Image, ImageAdmin)
