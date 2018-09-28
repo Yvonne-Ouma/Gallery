400b91046d25d6e025eb18ef19a6c57c681da6e8
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-28 11:37:25 -0400

    createed asearch button

--- gallery/models.py
+++ gallery/models.py
@@ -57,8 +57,8 @@ class Image(models.Model):
 
     @classmethod
     def search_by_category(cls,search_term):
-        posts = cls.objects.filter(category__icontains = search_term) 
-        return posts  
+        images = cls.objects.filter(category__icontains = search_term) 
+        return images  
 
 
 
