d5d325468cfde2650e20205a651f1df1bd0d4873
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-28 13:02:41 -0400

    routes are working

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url('^today/$',views.posts_today,name='postsToday'),
+    url('',views.posts_today,name='postsToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_posts,name = 'pastPosts'),
     url(r'^search/', views.search_results, name = 'search_results') 
     
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
