802f316af7bf3ff99cd05f01e7b12f847a462327
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-28 11:06:43 -0400

    added the url for the search functionality

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url('^today/$',views.posts_today,name='postsToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_posts,name = 'pastPosts') 
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_posts,name = 'pastPosts'),
+    url(r'^search/', view.search_results, name = 'search_results') 
     
 ]
